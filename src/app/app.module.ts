2971a57dfcd3fea21a5c307ff72f3db3f00473ca
hanaataif <hanaataif@gmail.com>
2020-03-18 19:56:23 +0100

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { InformationComponent } from './information/information.component';
+import { BoardComponent } from './board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    InformationComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
